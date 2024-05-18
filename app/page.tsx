8096b7347989e68f52b9d08180d95d4d8c137ac6
Sulav <suulaav@gmail.com>
2024-05-18 20:39:09 +0545

    Login page Ui completed

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,18 @@ export default function Home() {
                     <h1 className="glassy-title">Login</h1>
 
                     <div className="w-max"><label className="glassy-label">Email</label></div>
-                    <input className="border border-gray-400 rounded px-3 py-2 w-64 mb-3" placeholder="email" min="3" type="email"/>
+                    <input className="border border-gray-400 rounded px-3 py-2 w-64 mb-3" placeholder="email" min="3"
+                           type="email"/>
 
                     <div className="w-max"><label className="glassy-label">Password</label></div>
                     <input className="border border-gray-400 rounded px-3 py-2 w-64 mb-6" placeholder="password"
                            type="password" min="8"/>
 
-                    <button className="glassy-button" onClick={()=> alert("Invalid Credentials")}>
-                        Log In
-                    </button>
+                    <div className="w-full flex justify-center">
+                        <button className="glassy-button" onClick={() => alert("Invalid Credentials")}>
+                            Log In
+                        </button>
+                    </div>
                 </div>
             </div>
         </main>
